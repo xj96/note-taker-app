7ea63ec1af3761f9139bc91bbba42431d3af5670
Xavier Jackson <xavierejackson20@gmail.com>
2020-08-01 14:16:38 -0400

    added in dependencies

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Dependencies
 // ===============================================
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
 
 // Start Server
 // ===============================================
