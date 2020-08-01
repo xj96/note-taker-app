1c861fc3e18b613ff7d3af11c65694a7eb2685f4
Xavier Jackson <xavierejackson20@gmail.com>
2020-08-01 14:22:28 -0400

    Set up express

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const express = require("express")
 const path = require("path")
 const fs = require("fs")
 
+// Sets up Express App
+// ======================
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
 // Start Server
 // ===============================================
 
