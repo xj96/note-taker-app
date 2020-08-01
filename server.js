a48edfcfbad17aa34ba423b92509ed00fd89e711
Xavier Jackson <xavierejackson20@gmail.com>
2020-08-01 14:23:35 -0400

    set up express app to handle data parsing

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Start Server
 // ===============================================
